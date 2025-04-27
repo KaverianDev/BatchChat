2ca889a985c9f7d5257d480b18e122a7b8d9f302
Chicken <louh13192@duck.com>
2025-04-27 19:55:37 -0400

    Update finishupdate.bat

--- finishupdate.bat
+++ finishupdate.bat
@@ -1,6 +1,7 @@
 @echo on
 setlocal enabledelayedexpansion
-echo finishing update. do not close window.
+echo finishing update.
+echo do not close window.
 set sp=%programdata%\cmdchat
 if not exist %sp%\updatenew (
   echo there is no update to be finished.
@@ -10,9 +11,11 @@ if not exist %sp%\updatenew (
 del /q %sp%\updatenew
 set /p dir=<%sp%\cd
 del /q %sp%\cd
-cd %dir%
+cd !dir!
+pause
 del /q chat.bat
 ren chatnew.bat chat.bat
+pause
 echo update finished.
 echo starting new script.
 timeout /t 3 >nul
