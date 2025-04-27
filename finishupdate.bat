3ab4ccc624df371dbaf263e7cb562ad694ed6448
Chicken <louh13192@duck.com>
2025-04-27 19:49:04 -0400

    Update finishupdate.bat

--- finishupdate.bat
+++ finishupdate.bat
@@ -1,4 +1,21 @@
 @echo off
 setlocal enabledelayedexpansion
+echo finishing update. do not close window.
 set sp=%programdata%\cmdchat
-if not exist 
+if not exist %sp%\updatenew (
+  echo there is no update to be finished.
+  pause
+  exit
+)
+del /q %sp%\updatenew
+set /p dir=<%sp%\cd
+del /q %sp%\cd
+cd %dir%
+del /q chat.bat
+ren chatnew.bat chat.bat
+echo update finished.
+echo starting new script.
+timeout /t 3 >nul
+cls
+"chat.bat"
+endlocal
